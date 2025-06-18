c0c266e978d988b13700a9addefdf0f9eee6483e
rosemary53 <gulkcdgn13@gmail.com>
2025-06-18 15:44:56 +0300

    Kullanıcıdan alınan sayının 2 katını bulan kod

---
+++ IlkUygulama.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+
+/*
+ Öncelikle, kullanıcıdan bir tam sayı girmesini isteyen, bir tam sayı girmesini bekleyen ve ardından bu sayının 
+ 2 katının ne olduğunu söyleyen bir program oluşturalım. Program aşağıdaki çıktıyı üretmelidir.
+
+    Bir tam sayı girin: 5
+    Bu sayının iki katı: 10
+*/
+
+int main(){
+
+    int sayi;
+    std::cout << "Lutfen bir tam sayi giriniz : ";
+    std::cin >> sayi;
+    std::cout << "\nSiz tarafindan girilen sayi :" << sayi << "\n";
+    std::cout << "Girilen sayinin iki kati :" << (sayi * 2);
+
+    return 0;
+
+}
+
